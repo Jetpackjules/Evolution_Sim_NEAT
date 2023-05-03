3496c687f4282097eb893674877a40a98dea6d18
Jetpackjules <jetpackjules@gmail.com>
2023-05-03 15:28:16 -0700

    MAIN UPDATE!
    TREE IS NOW WORKING PROTOTYPE!! (no changes to other stuff)

--- demos/cars/tracks/track_4/Tree_Panel.gd
+++ demos/cars/tracks/track_4/Tree_Panel.gd
@@ -4,7 +4,7 @@ var species_array = []
 var radius_increment = 50
 
 # Use a fixed size for the map, but you can modify this according to your needs
-var map_size = Vector2(1000, 1000)
+var map_size = Vector2(5000, 5000)
 var angle_multiplier = 0.0
 var sorted_species_array = []
 
@@ -15,45 +15,10 @@ var font = load("res://NEAT_usability/fonts/dynamics/roboto-bold.tres")
 func _ready():
 	pass
 
-func draw_species_map(species_array):
-	var sample_species = [
-		{ "species_id": 1, "parent_id": null, "generation": 1, "color": Color(1, 0, 0), "angle": null },
-		{ "species_id": 2, "parent_id": null, "generation": 1, "color": Color(0, 1, 0), "angle": null },
-		{ "species_id": 3, "parent_id": null, "generation": 1, "color": Color(0, 0, 1), "angle": null },
-		{ "species_id": 4, "parent_id": null, "generation": 1, "color": Color(1, 1, 0), "angle": null },
-		{ "species_id": 5, "parent_id": null, "generation": 1, "color": Color(0, 1, 1), "angle": null },
-		{ "species_id": 6, "parent_id": null, "generation": 1, "color": Color(1, 0, 1), "angle": null },
-		{ "species_id": 7, "parent_id": null, "generation": 1, "color": Color(0.5, 0.5, 0.5), "angle": null },
-		{ "species_id": 8, "parent_id": null, "generation": 1, "color": Color(0.5, 0, 0), "angle": null },
-		{ "species_id": 9, "parent_id": null, "generation": 1, "color": Color(0, 0.5, 0), "angle": null },
-		{ "species_id": 10, "parent_id": null, "generation": 1, "color": Color(0, 0, 0.5), "angle": null },
+func draw_species_map(species_array_input):
 
-		{ "species_id": 11, "parent_id": 1, "generation": 2, "color": Color(1, 0.5, 0), "angle": null },
-		{ "species_id": 12, "parent_id": 1, "generation": 2, "color": Color(0.5, 1, 0), "angle": null },
-		{ "species_id": 13, "parent_id": 3, "generation": 2, "color": Color(0, 0.5, 1), "angle": null },
-		{ "species_id": 14, "parent_id": 4, "generation": 2, "color": Color(1, 1, 0.5), "angle": null },
-		{ "species_id": 15, "parent_id": 5, "generation": 2, "color": Color(0.5, 1, 1), "angle": null },
-		{ "species_id": 16, "parent_id": 10, "generation": 2, "color": Color(1, 0.5, 1), "angle": null },
-		{ "species_id": 17, "parent_id": 7, "generation": 2, "color": Color(0.5, 0.5, 0.5), "angle": null },
-		{ "species_id": 18, "parent_id": 10, "generation": 2, "color": Color(1, 0, 0.5), "angle": null },
-		{ "species_id": 19, "parent_id": 9, "generation": 2, "color": Color(0.5, 1, 0), "angle": null },
-		{ "species_id": 20, "parent_id": 10, "generation": 2, "color": Color(0, 0.5, 0.5), "angle": null },
-
-		{ "species_id": 21, "parent_id": 11, "generation": 3, "color": Color(1, 0.75, 0), "angle": null },
-		{ "species_id": 22, "parent_id": 12, "generation": 3, "color": Color(0.75, 1, 0), "angle": null },
-		{ "species_id": 23, "parent_id": 13, "generation": 3, "color": Color(0, 0.75, 1), "angle": null },
-		{ "species_id": 24, "parent_id": 14, "generation": 3, "color": Color(1, 1, 0.75), "angle": null },
-		{ "species_id": 25, "parent_id": 15, "generation": 3, "color": Color(0.75, 1, 1), "angle": null },
-		{ "species_id": 26, "parent_id": 16, "generation": 3, "color": Color(1, 0.75, 1), "angle": null },
-		{ "species_id": 27, "parent_id": 17, "generation": 3, "color": Color(0.75, 0.75, 0.75), "angle": null },
-		{ "species_id": 28, "parent_id": 18, "generation": 3, "color": Color(1, 0, 0.75), "angle": null },
-		{ "species_id": 29, "parent_id": 19, "generation": 3, "color": Color(0.75, 1, 0), "angle": null },
-		{ "species_id": 30, "parent_id": 20, "generation": 3, "color": Color(0, 0.75, 0.75), "angle": null }
-	]
-
+	species_array = species_array_input
 	
-#	species_array = sample_species
-	self.species_array = species_array
 	# Set the size of the control
 #	rect_min_size = map_size
 	rect_size = map_size
@@ -79,18 +44,30 @@ func _draw():
 		max_generations = max(max_generations, species.generation)
 
 	# Sort species by generation
-	species_array.sort_custom(self, "sort_by_generation")
+	species_array.sort_custom(self, "sort_by_ID")
 
 	# Draw the species on the map
 	var draw_radius = 0
-	for generation in range(max_generations + 1):
-		draw_radius += radius_increment
+	
+	var big_circle_color = Color(1, 0, 0)  # Red color
+	for gen_radius in range(radius_increment*2, (max_generations + 1) * radius_increment, radius_increment):
+		draw_circle(center, gen_radius, big_circle_color)
+		
+	for generation in range(1, max_generations + 1):
+		if generation == 1:
+			draw_radius += radius_increment*2
+		else:
+			draw_radius += radius_increment
 
 		var species_in_generation = []
 		for species in species_array:
 			if species.generation == generation:
 				species_in_generation.append(species)
 
+		var gen_size = species_in_generation.size()
+		print(gen_size)
+		
+		
 		for index in range(species_in_generation.size()):
 			var species = species_in_generation[index]
 			var parent_species = get_species_by_id(species.parent_id)
@@ -101,22 +78,47 @@ func _draw():
 
 			var total_children = species_in_generation.size()
 			var angle
+			var siblings_index = get_sibling_index(species.species_id)
+			var siblings = count_siblings(species.id)
 			if parent_species:
-				angle = calculate_angle(parent_angle, generation, index, total_children)
+#				angle = calculate_angle(parent_angle, generation, index, total_children, siblings, siblings_index)
+				angle = calculate_angle(parent_angle, generation, index, total_children, siblings, siblings_index, species.parent_id, draw_radius)
 			else:
-				angle = calculate_angle(0, generation, index, total_children)
+				angle = calculate_angle(0, generation, index, total_children, 0, 0, species.parent_id, draw_radius)
 
 			species.angle = angle
+#			Turning into radians:
+
+			angle *= (PI/180)
 			var pos = center + Vector2(cos(angle), sin(angle)) * draw_radius
 
-			draw_circle(pos, 10, species.color)
-			draw_string(font, pos - Vector2(5, -5), str(species.generation), species.color, 16)
 
+			var text = str(species.alive_members.size())
+			draw_circle(pos, 10, species.color, 2.0, true, species.obliterate, text)
 
 			# Draw lines connecting species to their ancestor
 			if parent_species:
+				parent_angle *= (PI/180)
 				var parent_pos = center + Vector2(cos(parent_angle), sin(parent_angle)) * (draw_radius - radius_increment)
-				draw_line(pos, parent_pos, species.color, 2.0)
+				# Calculate intersection points between parent and child circles
+				var parent_to_child = (pos - parent_pos).normalized()
+				var parent_intersection = parent_pos + parent_to_child * 10
+				var child_intersection = pos - parent_to_child * 10
+
+				draw_line(parent_intersection, child_intersection, species.color, 2.0)
+
+
+func calculate_required_angle(siblings, circle_radius):
+	if siblings == 0:
+		return 0
+	
+	var indicator_radius = 10.0
+	var max_indicator_width = (indicator_radius * 2) / circle_radius
+	var required_angle = 360 * max_indicator_width
+	
+	return required_angle
+
+
 
 func get_species_by_id(species_id):
 	for species in species_array:
@@ -124,10 +126,25 @@ func get_species_by_id(species_id):
 			return species
 	return null
 
-func sort_by_generation(species1, species2):
-	return species1.generation < species2.generation
+func sort_by_ID(species1, species2):
+	return int(species1.id) < int(species2.id)
 
+func count_siblings(species_id):
+	var target_species = get_species_by_id(species_id)
+	if not target_species:
+		return 0
 
+	var parent_id = target_species["parent_id"]
+	var generation = target_species["generation"]
+	var sibling_count = 0
+
+	if target_species["parent_id"] != "":
+		for species in species_array:
+			if species["parent_id"] == parent_id and species["species_id"] != species_id and species["generation"] == generation:
+				sibling_count += 1
+
+	return sibling_count
+	
 func get_species_count_by_generation(generation):
 	var count = 0
 	for species in species_array:
@@ -135,31 +152,76 @@ func get_species_count_by_generation(generation):
 			count += 1
 	return count
 
+func get_sibling_index(species_id):
+	var target_species = get_species_by_id(species_id)
+	if not target_species:
+		return 0
 
-func calculate_angle(parent_angle, generation, index, total_children):
-#	print("CALCULATING ANGLE!")
-#	parent_angle
-	var angle_offset = (360.0 / (total_children*(total_children+30)))
-	if index == 0:
-		print("ooo")
-	print(index)
+	var parent_id = target_species["parent_id"]
+	var siblings = []
+	
+	for species in species_array:
+		if species["parent_id"] == parent_id:
+			siblings.append(species)
+
+	return siblings.find(target_species)
+
+
+##OG WORKING WITH SLIGHT CROSS-PARENT OVERLAP:
+func calculate_angle(parent_angle, generation, index, total_children, siblings, siblings_index, parent_id, circle_radius):
+	var angle_offset = 360.0 / total_children
+
 	if generation == 1:
 		return angle_offset * index
 	else:
-		return fmod((parent_angle + (angle_offset + angle_multiplier) * index), 360)
+		# Calculate the angle range for the siblings
+		var sibling_angle_range = calculate_required_angle(siblings, circle_radius)
+
+		# Calculate the starting angle for the siblings
+		var start_angle = parent_angle - (sibling_angle_range / 2.0)
+
+		# Calculate the angle for the current sibling within the range
+		var angle
+		if siblings != 0:
+			angle = start_angle + (siblings_index * (sibling_angle_range / (siblings)))
+		else:
+			angle = parent_angle
+
+		# Adjust the angle based on the parent's angle
+		var adjusted_angle = angle
+
+		return fmod(adjusted_angle, 360)
 
 
 
 
-func draw_circle(position, radius, color):
+
+
+func draw_circle(position, radius, color, width = 2.0, is_filled = false, extinct = false, text="TREE"):
 	var num_segments = 32
 	var angle_increment = 2 * PI / num_segments
 	var prev_point = position + Vector2(cos(0), sin(0)) * radius
+	
+	if extinct:
+		color = Color(0.1, 0.1, 0.1, 1)
+
+	if is_filled:
+#		var filled_color = Color(color.r, color.g, color.b, fill_alpha)
+		var filled_color = Color(0,0,0,1)
+		for segment in range(1, num_segments + 1):
+			var angle = segment * angle_increment
+			var point = position + Vector2(cos(angle), sin(angle)) * radius
+			draw_colored_polygon([position, prev_point, point], filled_color)
+			prev_point = point
+
 	for segment in range(1, num_segments + 1):
 		var angle = segment * angle_increment
 		var point = position + Vector2(cos(angle), sin(angle)) * radius
-		draw_line(prev_point, point, color, 2.0)
+		draw_line(prev_point, point, color, width)
 		prev_point = point
+
+	var text_width = font.get_string_size(text).x
+	draw_string(font, position - Vector2(text_width/2, -5), text, color, 160)
 
 
 func _input(event):
--- demos/cars/tracks/track_4/tree.gd
+++ demos/cars/tracks/track_4/tree.gd
@@ -14,28 +14,8 @@ var orphan_nodes = {}
 var panel
 var panel_parent
 var panel_parent_OG_size
-	# Use the example creatures array
-class Creature:
-	var id
-	var parent1_id
-	var parent2_id
 
-	func _init(_id, parent1_id2, parent2_id2):
-		id = _id
-		parent1_id = parent1_id2
-		parent2_id = parent2_id2
-
-
-func generate_family(x):
-	var creatures = []
-	creatures.append(Creature.new(1, 0, 0))
-	creatures.append(Creature.new(2, 0, 0))
-	for i in range(3, x + 1):
-		var parent1_id = randi() % (i - 1) + 1
-		var parent2_id = randi() % (i - 1) + 1
-		creatures.append(Creature.new(i, parent1_id, parent2_id))
-	return creatures
-
+var scroll_container
 
 
 func _ready():
@@ -44,11 +24,13 @@ func _ready():
 	panel = $CanvasLayer/Control/FamilyTreeWindow/FamilyTreeScrollContainer/Control/Panel
 	panel_parent = $CanvasLayer/Control/FamilyTreeWindow/FamilyTreeScrollContainer/Control
 	panel_parent_OG_size = panel_parent.rect_size
+	scroll_container = panel_parent.get_parent()
 
 var zoom_level: float = 1.0
 var zoom_speed: float = 0.1
 var min_zoom: float = -10.0
 var max_zoom: float = 100.0
+
 
 func _input(event):
 	if event is InputEventKey:
@@ -62,17 +44,34 @@ func _input(event):
 				print("ZOOMING OUT")
 				zoom_level -= zoom_speed
 
-#			zoom_level = clamp(zoom_level, min_zoom, max_zoom)
+			# Get the current mouse position
+			var mouse_position = get_global_mouse_position()
 
-			var scroll_container = panel_parent.get_parent()
-			var zoom_factor = zoom_level / prev_zoom_level
+			# Calculate the local mouse position relative to the panel_parent
+			var local_mouse_position = mouse_position - panel_parent.rect_global_position
+
+			# Get the current scroll
+			var current_scroll = Vector2(scroll_container.scroll_horizontal, scroll_container.scroll_vertical)
+
+			# Calculate the scroll relative to the mouse position
+			var scroll_before_zoom = local_mouse_position + current_scroll
+
+			# Update the zoom level
+			panel.rect_scale = Vector2(zoom_level, zoom_level)
+			panel_parent.rect_min_size = panel_parent_OG_size * zoom_level
+
+			# Calculate the new scroll position
+			var scroll_after_zoom = scroll_before_zoom * (zoom_level / prev_zoom_level) - local_mouse_position
+
+			# Update the scroll_container properties
+			scroll_container.scroll_horizontal = scroll_after_zoom.x
+			scroll_container.scroll_vertical = scroll_after_zoom.y
 
 
-#			panel.rect_scale = Vector2(zoom_level, zoom_level)
-			panel_parent.rect_min_size = panel_parent_OG_size*zoom_level
 
-#			scroll_container.scroll_horizontal *= zoom_factor
-#			scroll_container.scroll_vertical *= zoom_factor
+
+
+
 
 func clear_family_tree():
 	for node in tree_nodes.values():
@@ -88,121 +87,16 @@ func clear_family_tree():
 	tree_nodes.clear()
 	orphan_nodes.clear()
 
-#func generate_family_tree(creatures):
-#	clear_family_tree()
-#	var creatures_dict = {}
-#
-#	creatures = generate_family(300)
-#	save_creatures_to_gedcom(creatures)
-#
-#	# Create nodes for all creatures
-#	for creature in creatures:
-#		var node = Node2D.new()
-#		node.z_index = 1
-#		node.name = "Creature_" + str(creature.id)
-#		node.position = Vector2(0, 0)
-#
-#		var label = Label.new()
-#		label.text = str(creature.id)
-#		label.modulate = Color.black
-#		node.add_child(label)
-#
-#		tree_nodes[creature.id] = node
-#		creatures_dict[creature.id] = creature
-#
-#		# Add the node as a child of the panel
-#		panel.add_child(node)
-#
-#	# Calculate positions of tree nodes
-#	var generation_spacing = 50
-#	var sibling_spacing = 300
-#	var x_offset = 300
-#	var y_offset = sibling_spacing * 2
-#
-#	# Create a dictionary to store the order of nodes within each generation
-#	var generation_order = {}
-#
-#	# Explicitly set the position for the first two nodes
-#	tree_nodes[1].position = Vector2(x_offset, y_offset)
-#	tree_nodes[2].position = Vector2(x_offset + sibling_spacing, y_offset)
-#
-#	for creature in creatures:
-#		if creature.parent1_id != 0 or creature.parent2_id != 0:
-#			var node = tree_nodes[creature.id]
-#
-#			# Calculate the generation number (depth in the tree)
-#			var generation = 1
-#			var current_creature = creature
-#			while current_creature.parent1_id != 0 or current_creature.parent2_id != 0:
-#				generation += 1
-#				current_creature = creatures_dict[current_creature.parent1_id]
-#
-#			# Update the order within the generation
-#			if not generation in generation_order:
-#				generation_order[generation] = 1
-#			else:
-#				generation_order[generation] += 1
-#
-#			# Calculate the position based on the generation number and order within the generation
-#			if generation > 1:
-#				node.position.x = x_offset + (generation - 1) * generation_spacing
-#				node.position.y = y_offset + (generation_order[generation] * 2 - generation) * sibling_spacing / 2
-#
-#	# Draw connections between parents and children
-#	draw_connections(creatures_dict)
-#
-#	# Setup zoom
-#	var max_x = 0
-#	var max_y = 0
-#
-#	for node_id in tree_nodes:
-#		max_x = max(max_x, tree_nodes[node_id].position.x)
-#		max_y = max(max_y, tree_nodes[node_id].position.y)
-#
-#	max_x += sibling_spacing + x_offset
-#	max_y += generation_spacing + y_offset
-#	print("SECOND Down-right-most node location: ", Vector2(max_x, max_y))
-#
-#	panel_parent.rect_min_size = Vector2(max_x, max_y + 50)
-#	panel.rect_size = Vector2(max_x, max_y + 50)
-#
-#func draw_connections(creatures_dict):  # Modify the parameter
-#	var line = Line2D.new()
-#	line.z_index = 0
-#	line.width = 1
-#	line.default_color = Color.white
-#	var material = SpatialMaterial.new()
-#	material.albedo_color = Color.blue
-#	line.material = material
-#	panel.add_child(line)
-#
-#	for creature_id in tree_nodes.keys():
-#		var creature = creatures_dict[creature_id]  # Change this line
-#		if creature.parent1_id != 0 and creature.parent1_id in tree_nodes:
-#			line.add_point(tree_nodes[creature.parent1_id].position)
-#			line.add_point(tree_nodes[creature.id].position)
-#
-#		if creature.parent2_id != 0 and creature.parent2_id in tree_nodes:
-#			line.add_point(tree_nodes[creature.parent2_id].position)
-#			line.add_point(tree_nodes[creature.id].position)
-
-
 func cycle(species):
-#	print("CYCLE!")
+
 	if $CanvasLayer/Control/FamilyTreeWindow.visible == false:
 		$CanvasLayer/Control/FamilyTreeWindow.popup_centered()
 		panel_parent_OG_size = panel_parent.rect_size
-#		clear_family_tree()
-#		var test_gens = generate_family(100)
+
 		panel.draw_species_map(species)
-#		panel.draw_family_tree(creatures)
+
 	else:
 		$CanvasLayer/Control/FamilyTreeWindow.visible = false
-#	print("------")
-#	print($CanvasLayer/Control/FamilyTreeWindow/FamilyTreeScrollContainer/Control.rect_min_size)
-#	print($CanvasLayer/Control/FamilyTreeWindow/FamilyTreeScrollContainer/Control/Panel.rect_min_size)
-#	print("----------")
-
 
 
 ##FOR PYTHON EXPORT:
